e5b732fa5a2eb424ddc28626b8de24e32cb2ed3e
gabrielalbert <gabrielalbert@live.com>
2025-01-23 19:38:45 +0530

    swagger ui fix

--- Startup.cs
+++ Startup.cs
@@ -64,7 +64,7 @@ namespace PromptEngineering
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PromptEngineering v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "PromptEngineering v1"));
             }
 
             app.UseHttpsRedirection();
